702c0b2281862f91d5065c6e515572a4fd297a60
fernandorocha-plank <fernando@crosswire.io>
2023-01-04 15:58:07 -0300

    Update task-02 description.

--- apps/mf-launches/src/pages/Home/index.tsx
+++ apps/mf-launches/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import LaunchesService, { ILaunchProperties } from '../../services/launches.service';
 
 //TODO #TASK-FRONTEND-02 Create a method with service callback with a ASC Alphabetical order
+// create a list component to receive de object retrieved from the api call
 
 const Home = () => {
   const [launches, setLaunches] = useState<ILaunchProperties[]>();
